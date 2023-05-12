b6ee7dfe996c1dd333e47f6028b3f71aa5898232
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-12 14:17:33 +0200

    Set up project files

---
+++ _app/js/modules/header.js
@@ -0,0 +1,17 @@
+export default function Header() {
+
+	window.onscroll = function() {stickyNavigation()};
+	
+	let header = document.querySelector(".header__content");
+	let content = document.querySelector(".header__content--sticky");
+	
+	let sticky = header.offsetTop;
+	
+	function stickyNavigation() {
+	  if (window.pageYOffset >= sticky) {
+		 header.classList.add("header__content--sticky")
+	  } else {
+		 header.classList.remove("header__content--sticky");
+	  }
+	}	
+}
