90ee4b2aec535ecdb9056ad90dd654a048d9097f
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-28 19:11:56 +0200

    Imported schemas

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,1 +1,9 @@
-export default []
+import settings from './documents/settings.js';
+import product from './documents/product.js';
+import category from './documents/category.js';
+
+export default [
+	settings,
+	product,
+	category,
+]
