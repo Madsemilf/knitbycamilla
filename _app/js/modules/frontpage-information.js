e7d1a00c154b5e87b8ad7c9cc79133144df135d1
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-29 11:53:44 +0200

    Rendered one image from pattern in DOM

--- _app/js/modules/frontpage-information.js
+++ _app/js/modules/frontpage-information.js
@@ -18,3 +18,18 @@ export default async function FrontpageInformationImage () {
 		const shuffledPatterns = array.sort(() => 0.5 - Math.random());
 		return shuffledPatterns.slice(0, count);
 	}
+
+	function createInformationImageDOM(pattern) {
+	  return `<div class="grid__column--6 frontpage-information-image">
+		 <img src="${pattern.Image.asset.url}" alt="Picture of different knitted products">
+	  </div>`;
+	}
+
+	function renderInformationImage () {
+		for (const pattern of selectedPattern) {
+			const patternImageItem = createInformationImageDOM(pattern);
+			informationImageContainer.innerHTML += patternImageItem;
+		}
+	}
+	renderInformationImage();
+ }
