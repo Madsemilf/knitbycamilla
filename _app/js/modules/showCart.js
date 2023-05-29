3ad93d5f7cf19a52bff0d8995a888fa24cd2c8a0
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-29 23:46:44 +0200

    Moved showcart.js to its own js file

---
+++ _app/js/modules/showCart.js
@@ -0,0 +1,45 @@
+export default function ShowCart() {
+
+	let cartBoxVisible = false;
+	let cartItemCount = 0;
+	
+
+	// Get the cart window and empty cart message elements
+	const cartButton = document.getElementById('navigation-cart-button')
+	const cartBox = document.getElementById('cart-box');
+	const cartEmptyMessage = document.getElementById('cart-empty-message');
+	const cartCountElement = document.querySelector('.navigation__cart-count');
+	
+
+	// Add event listeners to open and close the cart window
+	cartButton.addEventListener('click', handleCartButtonClick);
+
+	function handleCartButtonClick() {
+		toggleCartBox();
+		renderHTML();
+	}
+
+	function toggleCartBox() {
+		cartBoxVisible = !cartBoxVisible;
+	}
+
+	function renderHTML() {
+		if (cartBoxVisible === true) {
+			cartBox.classList.remove('cart-box--hide');
+		} else {
+			cartBox.classList.add('cart-box--hide')
+		}	
+	}
+
+
+
+	
+
+	// // Function to update the cart count
+	// function updateCartCount(count) {
+	// 	cartItemCount = count;
+	// 	cartCountElement.textContent = cartItemCount;
+ 	// }
+
+	//  updateCartCount(3)
+}
