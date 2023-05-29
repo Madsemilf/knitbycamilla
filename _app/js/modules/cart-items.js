9ab4d7065bbed082e6278c8f4f46fd5cfa6e1be6
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-29 23:36:03 +0200

    Created function to grab product data from Sanity

--- _app/js/modules/cart-items.js
+++ _app/js/modules/cart-items.js
@@ -5,3 +5,14 @@ export default function CartItems() {
 	const addToCartButtons = document.querySelectorAll('.products__product-add-to-cart')
 	const cartEmptyButton = document.querySelector('.cart-box__button--empty');
 	const cartCheckoutButton = document.querySelector('.cart-box__checkout-button');
+
+	function addToCart(button) {
+		const clickedButtonDataset = button.parentNode.dataset;
+		const product = {
+			id: clickedButtonDataset.id,
+			name: clickedButtonDataset.name,
+			price: clickedButtonDataset.price,
+			quantity: 1, 
+		}
+		
+		const matchInCart = cartProducts.find(product => product.id === clickedButtonDataset.id)
