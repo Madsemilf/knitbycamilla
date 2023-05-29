d182eeb05c4f117dae258716baa0fe9112618017
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-29 23:48:22 +0200

    Removed unused and commented out code

--- _app/js/modules/patternProducts.js
+++ _app/js/modules/patternProducts.js
@@ -13,33 +13,6 @@ export default async function PatternProducts() {
 	const patterns = await sanity.fetch(query);
 
 	if (document.getElementById('pattern-products')) {
-
-		// function createPatternItemDOM(pattern_document) {
-		// 	const product = document.createElement('div');
-		// 	const productImage = document.createElement('div');
-		// 	const productImg = document.createElement('img');
-		// 	const productInformation = document.createElement('div');
-		// 	const productName = document.createElement('div');
-		// 	const productPrice = document.createElement('div');
-		// 	const productAddToCartButton = document.createElement('button');
-	
-		// 	// return product;
-		// }
-	
-		// function createPatternItemDOMTheWrongWay(pattern_document) {
-		// 	return `<div class="frontpage-products__product box grid__column--3" data-id="${pattern_document._id}" data-name="${pattern_document.name}" data-price="${product_document.price}">
-		// 		<div class="frontpage-products__product-image">
-		// 			<img class="frontpage-products__product-img" src="${pattern_document.preview.asset.url}?w=800" alt="">
-		// 		</div>
-	
-		// 		<div class="frontpage-products__product-information">
-		// 			<div class="frontpage-products__product-name">${pattern_document.name}</div>
-		// 			<div class="frontpage-products__product-price">${formatPrice(pattern_document.price)}</div>
-		// 		</div>
-	
-		// 		<button class="frontpage-products__product-add-to-cart-button input-button">Add to Cart</button>
-		// 	</div>`;
-		// }
 	
 		function createPatternItemDOM(pattern_document) {
 		const product = document.createElement('div');
@@ -80,27 +53,6 @@ export default async function PatternProducts() {
 		return product;
 		}
 	
-		// <!-- <div class="products__product box grid__column--4" data-id="${product_document._id}" data-name="${product_document.name}" data-price="${product_document.price}">
-		// 			<div class="products__product-image">
-		// 				<img class="products__product-img" src="${product_document.Image.asset.url}" alt="">
-		// 			</div>
-	
-		// 			<div class="products__product-information">
-		// 				<div class="products__product-name">${product_document.name}</div>
-		// 				<div class="products__product-price">${product_document.price}</div>
-		// 			</div>
-	
-		// 			<button class="products__product-add-to-cart input-button">Add to Cart</button>
-		// 		</div>
-	
-	
-		// function insertPatternItemsTheWrongWay() {
-		// 	for (const pattern of patterns) {
-		// 		const productItem = createPatternItemDOMTheWrongWay(pattern);
-		// 		patternsContainer.innerHTML += productItem;
-		// 	}
-		// }
-	
 		function insertPatternItems() {
 			for (const pattern of patterns) {
 			  const patternItem = createPatternItemDOM(pattern);
@@ -115,5 +67,5 @@ export default async function PatternProducts() {
 	
 		render();
 	}
-	
 }
+
