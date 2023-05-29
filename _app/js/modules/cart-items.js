ef42280a7e8d2b72d7a65bba92a3a9b9fe10e4d7
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-29 23:34:49 +0200

    Added variables for grabbing my dom elements

---
+++ _app/js/modules/cart-items.js
@@ -0,0 +1,6 @@
+	const cartContainer = document.querySelector('.cart-box')
+	const emptyCartMessage = document.querySelector('.cart-box__empty-message--visible')
+	const cartBoxContent = document.querySelector('.cart-box__content');
+	const addToCartButtons = document.querySelectorAll('.products__product-add-to-cart')
+	const cartEmptyButton = document.querySelector('.cart-box__button--empty');
+	const cartCheckoutButton = document.querySelector('.cart-box__checkout-button');
