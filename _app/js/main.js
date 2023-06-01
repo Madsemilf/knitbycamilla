30166a557c36c8c0463dc16031fdd20332dd78be
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-06-01 20:27:54 +0200

    Removed unused functions

--- _app/js/main.js
+++ _app/js/main.js
@@ -4,12 +4,9 @@ import FrontpageCategories from './modules/frontpage-categories.js'
 import FrontpageHeroImages from './modules/frontpage-hero-images.js'
 import FrontpageInformationImage from './modules/frontpage-information.js'
 import PatternProducts from './modules/patternProducts.js'
-import CartItems from './modules/cart-items.js'
-
 
 ShowCart ();
 FrontpageCategories ();
 FrontpageHeroImages ();
 FrontpageInformationImage ();
 await PatternProducts ();
-// CartItems();
