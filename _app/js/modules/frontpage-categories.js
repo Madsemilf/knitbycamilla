319ace3489818e8d1ab3ead47d4a983dc6164ec1
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-06-01 21:27:06 +0200

    Changed how to render frontpage categories

--- _app/js/modules/frontpage-categories.js
+++ _app/js/modules/frontpage-categories.js
@@ -1,7 +1,7 @@
 import { sanity } from '../sanity.js'
 
 export default async function FrontpageCategories () {
-	const categoryContainer = document.querySelector('.frontpage-categories')
+	const frontpageCategoryContainer = document.querySelector('.frontpage-categories')
 	const query = `*[_type == 'category'] {
 		...,
 		preview {
@@ -13,29 +13,62 @@ export default async function FrontpageCategories () {
 	const categories = await sanity.fetch(query);
 
 	if (document.body.classList.contains('home-page')) {
-		function createCategoryImageDOM(category) {
 
-			return `<div class="frontpage-catgories__category grid__column--3" data-name:"${category.name}">
-			<a href="/pattern/index.html" class="frontpage-categories-link">
-			<button class="frontpage-categories-image">
-				<img src="${category.preview.asset.url}" alt="">
-			</button>
-			<div>
-				<button class="frontpage-catgories__category--button">${category.name}</button>
-			</div>
-			</a>
-		</div>`
+		function createCategoryImageDOM(category) {
+			
+			const frontpageCategoryItem = document.createElement('div');
+			const frontpageCategoryLink = document.createElement('a');
+			const frontpageCategoryImageButton = document.createElement('button');
+			const frontpageCategoryImage = document.createElement('img');
+			const frontpageCategoryName = document.createElement('div');
+			const frontpageCategoryButton = document.createElement('button');
+
+			frontpageCategoryItem.setAttribute('data-name', category.name);
+
+			frontpageCategoryItem.classList.add('frontpage-catgories__category', 'grid__column--3');
+			frontpageCategoryImageButton.classList.add('frontpage-categories-image');
+			frontpageCategoryLink.classList.add('frontpage-categories-link');
+			frontpageCategoryButton.classList.add('frontpage-catgories__category--button');
+
+			frontpageCategoryLink.href = '/pattern/index.html';
+			frontpageCategoryImage.src = category.preview.asset.url;
+			frontpageCategoryImage.alt = '';
+			frontpageCategoryButton.textContent = category.name;
+			
+			frontpageCategoryContainer.appendChild(frontpageCategoryItem);
+			frontpageCategoryItem.appendChild(frontpageCategoryLink);
+			frontpageCategoryLink.appendChild(frontpageCategoryImageButton);
+			frontpageCategoryImageButton.appendChild(frontpageCategoryImage);
+			frontpageCategoryLink.appendChild(frontpageCategoryName);
+			frontpageCategoryName.appendChild(frontpageCategoryButton);
 		}
-	
-	
-		function renderCategoryImages () {
+
+		function renderCategoryImages() {
 			for (const category of categories) {
-				const categoryItem = createCategoryImageDOM(category);
-				// categoryContainer.appendChild(categoryItem)
-	
-				categoryContainer.innerHTML += categoryItem;
+			createCategoryImageDOM(category);
 			}
 		}
 		renderCategoryImages();
 	}
 }
+
+// 	return `<div class="frontpage-catgories__category grid__column--3" data-name:"${category.name}">
+		// 	<a href="/pattern/index.html" class="frontpage-categories-link">
+		// 	<button class="frontpage-categories-image">
+		// 		<img src="${category.preview.asset.url}" alt="">
+		// 	</button>
+		// 	<div>
+		// 		<button class="frontpage-catgories__category--button">${category.name}</button>
+		// 	</div>
+		// 	</a>
+		// </div>`
+		// }
+	
+	
+		// function renderCategoryImages () {
+		// 	for (const category of categories) {
+		// 		const categoryItem = createCategoryImageDOM(category);
+		// 		// categoryContainer.appendChild(categoryItem)
+	
+		// 		categoryContainer.innerHTML += categoryItem;
+		// 	}
