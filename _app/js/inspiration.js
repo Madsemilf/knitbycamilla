bac9120ea30c5c2fec68b1c33fa49621f24a745b
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-06-02 02:30:00 +0200

    Created inspiration.js

---
+++ _app/js/inspiration.js
@@ -0,0 +1,7 @@
+import StaticPage from './modules/static-page.js';
+import ShowCart from './modules/showCart.js'
+import RenderInspirationPage from './modules/renderInspiration.js'
+
+await StaticPage('inspiration');
+ShowCart();
+RenderInspirationPage();
