5e81993e07eaffde74d7ea4eb303c36bef9c0710
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-06-02 02:31:07 +0200

    Added schema for page

---
+++ _studio/schemas/documents/page.js
@@ -0,0 +1,25 @@
+export default {
+	title: 'Page',
+	name: 'page',
+	type: 'document',
+	fields: [
+		{
+			title: 'Title',
+			name: 'title',
+			type: 'string',
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			options: {
+				source: 'title'
+			}
+		},
+		{
+			title: 'Content',
+			name: 'content',
+			type: 'text',
+		}
+	]
+}
--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -2,10 +2,12 @@ import settings from './documents/settings.js';
 import pattern from './documents/pattern.js';
 import category from './documents/category.js';
 import account from './documents/account.js';
+import page from './documents/page.js'
 
 export default [
 	settings,
 	pattern,
 	category,
 	account,
+	page
 ]
