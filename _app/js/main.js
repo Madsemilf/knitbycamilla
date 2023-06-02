e32a719db0e54164955c3f9d18cffdd14e0130c3
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-06-02 02:30:12 +0200

    Removed a line

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,4 +1,3 @@
-
 import ShowCart from './modules/showCart.js'
 import FrontpageCategories from './modules/frontpage-categories.js'
 import FrontpageHeroImages from './modules/frontpage-hero-images.js'
